269af754aa0e63defeda5c022710ebb35618d7a4
astefanov83 <152632321+astefanov83@users.noreply.github.com>
2024-04-18 10:19:17 -0400

    First push

---
+++ main.py
@@ -0,0 +1,17 @@
+
+def init_func():
+    func1 = input("Enter your function f(x):")
+    ...
+
+def take_integral():
+    ...
+
+def print_graph():
+    ...
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
