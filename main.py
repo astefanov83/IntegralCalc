f850769b16cb70fbddec8caba3cabb1462854f48
astefanov83 <152632321+astefanov83@users.noreply.github.com>
2024-04-22 10:39:16 -0400

    added Integral function

--- main.py
+++ main.py
@@ -11,24 +11,41 @@ class Integral:
         func1 = input("Enter your function f(x):")
         x1bound = input("Enter the lower bound of the integral:")
         x2bound = input("Enter the upper bound of the integral:")
-        ...
+        
+        return func1, x1bound, x2bound
 
     def take_integral(x1bound, x2bound, func1):
-        sum = 0
-        for x in range(x1bound, x2bound, (x2bound - x1bound)/1000):
-            sum += func1(x) * ((x2bound - x1bound)/1000)
 
-        return sum
+        curr2 = x1bound
+        tot = 0
 
-        
-        ...
+        for i in range(0, 10000):
+
+            step = float((x2bound - x1bound)/10000)
+
+            copyoffunc = func1
+            curr = 0
+
+            for letter in range(len(copyoffunc)):
+                if copyoffunc[letter] == 'x': 
+                    copyoffunc = copyoffunc[:letter] + str(curr2 + step) + copyoffunc[letter + 1:]
+
+            curr2 += step
+            curr = (eval(copyoffunc))*(step)
+            print("ran")
+            tot += curr
+
+        return tot
 
     def print_graph():
         ...
 
 def main():
-    ...
+    func, lo, hi = Integral.init_func()
 
+    print(Integral.take_integral(int(lo), int(hi), func))
+
+    
 
 if __name__ == '__main__':
     main()
---
+++ test.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+
+# find out how to orient tkinter for graphing
+
+root = Tk()
+
+canvas = Canvas(parent width=500, height=400)
+
+canvas.create_line(10,5, 200, 50)
+
+root.mainloop()
